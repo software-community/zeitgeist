e7b9aec4804ad821b68cd270d0fdf228226d9976
divyanshu14 <iamavi53@gmail.com>
2019-07-08 15:36:27 +0530

    Removed virtual env. Make your own using requirements.txt

--- zeitgeist_2019_virtualenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\divyanshu\desktop\zeitgeist_2019\zeitgeist_2019_virtualenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
