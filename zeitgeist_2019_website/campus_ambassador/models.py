6c90da5a52d317c82cdae530a9b2cbd287ffd3b7
divyanshu14 <iamavi53@gmail.com>
2019-07-12 23:08:11 +0530

    Final changes and tweaks

--- zeitgeist_2019_website/campus_ambassador/models.py
+++ zeitgeist_2019_website/campus_ambassador/models.py
@@ -4,14 +4,13 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class RegistrationDetails(models.Model):
-    college = models.CharField(max_length=200)
+    college = models.CharField(max_length=200, verbose_name='College Name')
     # would have liked to use PhoneNumberField from phonenumber_field.modelfields
     # pip install django-phonenumber-field
     # pip install phonenumbers (or) pip install phonenumberslite
-    mobile_number = models.CharField(max_length=15)
-    why_interested = models.TextField()
-    past_experience = models.TextField()
-    accept_campus_ambassador_policy = models.BooleanField(default=False)
+    mobile_number = models.CharField(max_length=15, verbose_name='Mobile Number')
+    why_interested = models.TextField(verbose_name='Why do you want to be a Campus Ambassador?')
+    past_experience = models.TextField(verbose_name='Do you have any past experience related to this? If yes, then please share your experience')
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
 
     class Meta:
--- zeitgeist_2019_website/main_page/views.py
+++ zeitgeist_2019_website/main_page/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import logout
+from django.urls import reverse
+from urllib.parse import urlparse
 # Create your views here.
 
 def home(request):
     return render(request, 'main_page/index.html')
 
-def change_email(request):
+# if some other page redirects us to change_account, then 404 will happen
+# but why will some other page redirect to here
+def change_account(request):
+    prev_page = request.META.get('HTTP_REFERER', reverse('main_page_home'))
+    _next = urlparse(prev_page).path
     logout(request)
-    return redirect('account_login')
+    return redirect(reverse('google_login')+'?next='+_next)
