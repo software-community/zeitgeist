417c60318aa4099b0bbf7672ec4cb89ff176f89a
divyanshu14 <iamavi53@gmail.com>
2019-08-11 15:57:15 +0530

    Slight changes to database

--- zeitgeist_2019_website/main_page/admin.py
+++ zeitgeist_2019_website/main_page/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import SubCategory, Event, Participant, ParticipantHasPaid, ParticipantHasParticipated, Team, TeamHasMember
 
 # Register your models here.
+
+# class SubCategoryAdmin(admin.ModelAdmin):
+#     list_display = [field.name for field in SubCategory._meta.get_fields()]
+
+# class EventAdmin(admin.ModelAdmin):
+#     list_display = [field.name for field in Event._meta.get_fields()]
+
+# class ParticipantAdmin(admin.ModelAdmin):
+#     list_display = [field.name for field in Participant._meta.get_fields()]
+
+# class ParticipantHasPaidAdmin(admin.ModelAdmin):
+#     list_display = [field.name for field in ParticipantHasPaid._meta.get_fields()]
+
+# class ParticipantHasParticipatedAdmin(admin.ModelAdmin):
+#     list_display = [field.name for field in ParticipantHasParticipated._meta.get_fields()]
+
+# class TeamAdmin(admin.ModelAdmin):
+#     list_display = [field.name for field in Team._meta.get_fields()]
+
+# class TeamHasMemberAdmin(admin.ModelAdmin):
+#     list_display = [field.name for field in TeamHasMember._meta.get_fields()]
+
+# admin.site.register(SubCategory, SubCategoryAdmin)
+# admin.site.register(Event, EventAdmin)
+# admin.site.register(Participant, ParticipantAdmin)
+# admin.site.register(ParticipantHasPaid, ParticipantHasPaidAdmin)
+# admin.site.register(ParticipantHasParticipated, ParticipantHasParticipatedAdmin)
+# admin.site.register(Team, TeamAdmin)
+# admin.site.register(TeamHasMember, TeamHasMemberAdmin)
+
+admin.site.register(SubCategory)
+admin.site.register(Event)
+admin.site.register(Participant)
+admin.site.register(ParticipantHasPaid)
+admin.site.register(ParticipantHasParticipated)
+admin.site.register(Team)
+admin.site.register(TeamHasMember)
--- zeitgeist_2019_website/main_page/models.py
+++ zeitgeist_2019_website/main_page/models.py
@@ -1,3 +1,123 @@
 from django.db import models
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+
+
+class SubCategory(models.Model):
+
+    name = models.CharField(max_length=40, null=False, blank=False)
+
+    CATEGORY_CHOICES = [
+        ('Dance', 'Dance'),
+        ('Dramatics', 'Dramatics'),
+        ('Fine Arts', 'Fine Arts'),
+        ('Gaming', 'Gaming'),
+        ('Life Style', 'Life Style'),
+        ('Literary', 'Literary'),
+        ('Music', 'Music'),
+        ('Photography', 'Photography'),
+        ('Quizzing', 'Quizzing'),
+        ('Video Making', 'Video Making'),
+    ]
+    category = models.CharField(
+        max_length=20,
+        choices=CATEGORY_CHOICES,
+        null=False,
+        blank=False,
+    )
+
+    participation_fees_per_person = models.IntegerField(blank=False)
+
+    def __str__(self):
+        return str(self.name) + " - " + str(self.category)
+
+    class Meta:
+        verbose_name_plural = 'Sub Categories'
+
+
+class Event(models.Model):
+
+    name = models.CharField(max_length=40, null=False, blank=False)
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+
+    EVENT_TYPE_CHOICES = [
+        ('Solo', 'Solo'),
+        ('Duet', 'Duet'),
+        ('Group', 'Group'),
+    ]
+    event_type = models.CharField(
+        max_length=5,
+        choices=EVENT_TYPE_CHOICES,
+        null=False,
+        blank=False,
+    )
+
+    first_cash_prize = models.IntegerField(blank=False)
+    second_cash_prize = models.IntegerField(blank=False)
+    third_cash_prize = models.IntegerField(blank=False)
+    minimum_team_size = models.IntegerField(blank=False)
+    maximum_team_size = models.IntegerField(blank=False)
+    description = models.TextField(blank=False)
+
+    def __str__(self):
+        return str(self.name) + " - " + str(self.sub_category)
+
+
+class Participant(models.Model):
+
+    participating_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    mobile_number = PhoneNumberField(verbose_name='Mobile Number', region='IN')
+
+    def __str__(self):
+        return str(self.participating_user)
+
+
+class ParticipantHasPaid(models.Model):
+
+    participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    paid_sub_category = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    transaction_id = models.CharField(max_length=100, default='-1')
+
+    def __str__(self):
+        return str(self.participant) + " - " + str(self.paid_sub_category)
+
+    class Meta:
+        verbose_name_plural = 'Participant Has Paid'
+
+
+class ParticipantHasParticipated(models.Model):
+
+    participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.participant) + " - " + str(self.event)
+
+    class Meta:
+        verbose_name_plural = 'Participant Has Participated'
+
+
+class Team(models.Model):
+
+    name = models.CharField(max_length=40, null=False, blank=False)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    captain = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    primary_mobile_number = PhoneNumberField(null=False, blank=False, verbose_name='Primary Mobile Number', region='IN')
+    secondary_mobile_number = PhoneNumberField(null=False, blank=False, verbose_name='Secondary Mobile Number', region='IN')
+
+    def __str__(self):
+        return str(self.name) + " - " + str(self.event) + " - " + str(self.captain)
+
+
+class TeamHasMember(models.Model):
+
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    member = models.ForeignKey(Participant, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.team) + " - " + str(self.member)
+
+    class Meta:
+        verbose_name_plural = 'Team Has Member'
