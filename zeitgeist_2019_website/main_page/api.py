a3f4171d0b63322267c26313267643e8b73295ae
dilip640 <dilipsharma640@gmail.com>
2019-08-23 21:20:50 +0530

    Fix DATA-API

--- zeitgeist_2019_website/main_page/api.py
+++ zeitgeist_2019_website/main_page/api.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .serializers import CategorySerializer, SubcategorySerializer, EventSerializer
 from .models import Category, Subcategory, Event
@@ -8,13 +9,19 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     http_method_names = ['get']
+    filter_backends = (DjangoFilterBackend, )
+    filter_fields = ('name', )
 
 class SubcategoryViewSet(viewsets.ModelViewSet):
     queryset = Subcategory.objects.all()
     serializer_class = SubcategorySerializer
     http_method_names = ['get']
+    filter_backends = (DjangoFilterBackend, )
+    filter_fields = ('name', 'category')
 
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
     http_method_names = ['get']
+    filter_backends = (DjangoFilterBackend, )
+    filter_fields = ('name', 'subcategory')
