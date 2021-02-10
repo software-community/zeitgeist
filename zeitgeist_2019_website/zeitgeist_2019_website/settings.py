2adc96b5fe6772d11eb53df72962359a2c318014
Paras Goyal <parasgoyal12@gmail.com>
2021-02-10 19:57:16 +0530

    Modify Settings

--- zeitgeist_2019_website/zeitgeist_2019_website/settings.py
+++ zeitgeist_2019_website/zeitgeist_2019_website/settings.py
@@ -173,9 +173,9 @@ LOGOUT_REDIRECT_URL = 'main_page_home'
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_PORT = '587'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
