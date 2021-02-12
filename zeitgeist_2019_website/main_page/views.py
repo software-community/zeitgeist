efc6c099d5dafacbd0f08d6e972384f11c0aba01
Paras Goyal <parasgoyal12@gmail.com>
2021-02-12 14:09:35 +0530

    Testing webhook

--- zeitgeist_2019_website/main_page/views.py
+++ zeitgeist_2019_website/main_page/views.py
@@ -276,7 +276,8 @@ def weebhook(request):
             data.items(), key=lambda x: x[0].lower()))
         mac_calculated = hmac.new(
             (os.getenv('private_salt')).encode('utf-8'), message.encode('utf-8'), hashlib.sha1).hexdigest()
-
+        print(mac_calculated)
+        print(mac_provided)
         if mac_provided == mac_calculated:
             try:
                 participantpaspaid = ParticipantHasPaid.objects.get(
