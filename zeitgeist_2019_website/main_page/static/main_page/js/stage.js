f685b684f675519a2bdaea059adff42ffafd3cba
Yash Mittal <23.yashm@gmail.com>
2021-04-22 15:45:06 +0530

    stage animation bug fix

--- zeitgeist_2019_website/main_page/static/main_page/js/stage.js
+++ zeitgeist_2019_website/main_page/static/main_page/js/stage.js
@@ -1,32 +1,31 @@
 $('.arrow').click(function(){
-  $('.stage-front').css('margin-top','-5%')
-  $('.stage-front').css('margin-left','-10%')
+  $('.stage-front-wrap').css('transform','translateY(0) translateX(0)')
   $('.but-cont').css('opacity','0')
 })
 
 $('.but1').click(function(){
-  $('.stage-front').css('margin-top','100vh')
+  $('.stage-front-wrap').css('transform','translateY(100vh)')
   setTimeout(function(){
   $('.but1-cont').css('opacity','1')
   },400);
 })
 
 $('.but2').click(function(){
-  $('.stage-front').css('margin-top','-100vh')
+  $('.stage-front-wrap').css('transform','translateY(-100vh)')
   setTimeout(function(){
   $('.but2-cont').css('opacity','1')
   },400);
 })
 
 $('.but3').click(function(){
-  $('.stage-front').css('margin-left','100vw')
+  $('.stage-front-wrap').css('transform','translateX(100vw)')
   setTimeout(function(){
   $('.but3-cont').css('opacity','1')
   },400);
 })
 
 $('.but4').click(function(){
-  $('.stage-front').css('margin-left','-110vw')
+  $('.stage-front-wrap').css('transform','translateX(-100vw)')
   setTimeout(function(){
   $('.but4-cont').css('opacity','1')
   },400);
